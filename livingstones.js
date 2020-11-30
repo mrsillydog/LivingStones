089aba95c8d340a9033db27aa896d8c7ba9940d5
mrsillydog <ian.sam.adams@gmail.com>
2020-11-30 16:37:19 -0500

    Documentation, silly bug fix, result formatting

--- livingstones.js
+++ livingstones.js
@@ -1,12 +1,20 @@
+/*
+ * livingstones.js contains the javascript methods for Living Stones Academy's tuition calculator.
+ * Written by github.com/mrsillydog, Fall 2020
+ */
 
-
-function preparehtml() {
+/*
+ * proceedCalc dynamically adds <select> html elements to the page based on the 
+ * number of children at LSA entered by the user.
+ * Triggered by clicking the <button> with ID proceed
+ */
+function proceedCalc() {
     var numStudents = document.getElementById("number-at-lsa").value;
     var table = document.getElementById("studentsTable");
     var tableInstructions = document.getElementById("tableInstructions");
     var calcButton = document.getElementById("calculateButton");
-    table.removeAttribute("hidden");
     tableInstructions.style.display = "block";
+    table.style.display="block";
     calcButton.style="";
     var tableHTML = "";
     for (var i = 0; i < numStudents; i++){
@@ -30,34 +38,24 @@ function preparehtml() {
     table.innerHTML = tableHTML;
 }
 
-function reset() {
+/*
+ * resetProceedCalc essentially reverses the effects of proceedCalc
+ * triggered whenever the user changes the value in the <select> element with ID number-at-lsa
+ */
+function resetProceedCalc() {
     var table = document.getElementById("studentsTable");
     var tableInstructions = document.getElementById("tableInstructions");
     var calcButton = document.getElementById("calculateButton");
-    var preschoolResults = document.getElementById("preschoolresults");
-    var k_6Results = document.getElementById("K_6results");
-    var overallResults = document.getElementById("finalresults");
-    if (!table.hasAttribute("hidden")) {
-        table.toggleAttribute("hidden");
-    }
-    if (!tableInstructions.hasAttribute("hidden")) {
-        tableInstructions.toggleAttribute("hidden");
-    }
-    if (!calcButton.hasAttribute("hidden")) {
-        calcButton.toggleAttribute("hidden");
-    }
-    if (!preschoolResults.hasAttribute("hidden")) {
-        preschoolResults.toggleAttribute("hidden");
-    }
-    if (!k_6Results.hasAttribute("hidden")) {
-        k_6Results.toggleAttribute("hidden");
-    }
-    if (!overallResults.hasAttribute("hidden")) {
-        overallResults.toggleAttribute("hidden");
-    }
+    tableInstructions.style.display = "none";
+    table.style.display = "none";
+    calcButton.style.display = "none";
 }
 
-function calculate() {
+/*
+ * calculateTuition displays full tuition, savings, adjusted tuition, monthly cost, and weekly cost based on user inputs.
+ * Triggered by clicking the <button> with ID calculateButton
+ */
+function calculateTuition() {
     // prep
     const K_6 = -1;
     var agi = document.getElementById("tuition").value;
@@ -86,9 +84,9 @@ function calculate() {
     // actually calculate K-6 tuition
     var k_6Tuition = 0;
     if (howManyK_6 > 1) {
-        k_6Tuition = k_6(agi, fig, 1);
+        k_6Tuition = k_6Calc(agi, fig, 1);
     } else if (howManyK_6 == 1) {
-        k_6Tuition = k_6(agi, fig, 0);
+        k_6Tuition = k_6Calc(agi, fig, 0);
     }
 
     // alter value based on minimum tuition value of $600
@@ -97,9 +95,9 @@ function calculate() {
     // actually calculate preschool tuition
     var preschoolTuition = 0;
     if (howManyPreschool > 1) {
-        preschoolTuition = preschool(agi, fig, 1);
+        preschoolTuition = preschoolCalc(agi, fig, 1);
     } else if (howManyPreschool == 1) {
-        preschoolTuition = preschool(agi, fig, 0);
+        preschoolTuition = preschoolCalc(agi, fig, 0);
     }
     
     // check to see if agi is > 200% fig and adapt accordingly
@@ -126,43 +124,61 @@ function calculate() {
             totalTuition = preschoolTuition;
         }
     }
+
+    // calculate values to display
     var originalTotal = originalPreschool + originalK_6;
     var savingsTotal = originalTotal - totalTuition;
     var totalResults = document.getElementById("finalresults");
 
+    // display
     totalResults.innerHTML = 
     `
     <th>Tuition Calculation Results</th>
     <tr>
-        <td>Pre-aid Tuition</td>
-        <td>` + originalTotal.toFixed(2) + `</td>
+        <td>Full Tuition</td>
+        <td>$` + numberWithCommas(originalTotal.toFixed(2)) + `</td>
     </tr>
     <tr>
-        <td>Estimated Financial Aid</td>
-        <td>` + savingsTotal.toFixed(2) + `</td>
+        <td>Estimated Tuition Assistance</td>
+        <td>$` + numberWithCommas(savingsTotal.toFixed(2)) + `</td>
     </tr>
     <tr>
-        <td>Estimated Net Cost</td>
-        <td>` + totalTuition.toFixed(2) + `</td>
+        <td>Your Estimated Tuition</td>
+        <td>$` + numberWithCommas(totalTuition.toFixed(2)) + `</td>
     </tr>
     <tr>
         <td>Estimated Monthly Payment
             <span class="description">Based on a 10-month payment plan.</span>
         </td>
-        <td>` + (totalTuition / 10).toFixed(2) + `</td>
+        <td>$` + numberWithCommas((totalTuition / 10).toFixed(2)) + `</td>
     </tr>
     <tr>
         <td>Estimated Weekly Payment
             <span class="description">Based on a 10-month payment plan, with 4 weeks per month.</span>
         </td>
-        <td>` + (totalTuition / 40).toFixed(2) + `</td>
+        <td>$` + numberWithCommas((totalTuition / 40).toFixed(2)) + `</td>
     </tr>`;
 
-    toggle();
+    toggleCalc();
 }
 
+/*
+ * Thanks to Elias Zamaria, @neu-rah, and T.J Crowder from 
+ * https://stackoverflow.com/questions/2901102/how-to-print-a-number-with-commas-as-thousands-separators-in-javascript
+ * for this shamelessly copied utility method.
+ * Converts a integer value to a formatted string through clever RegEx matching.
+ */
+function numberWithCommas(x) {
+    return x.toString().replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ",");
+}
 
-function k_6(agi, fig, multiple) {
+/*
+ * k_6Calc returns a family tuition value for K-6 tuition based on:
+ *  - the family's adjusted gross income
+ *  - federal income guidelines
+ *  - whether they have multiple children attending K-6 at LSA (1 or 0)
+ */
+function k_6Calc(agi, fig, multiple) {
     var multiple_modifier = multiple * 0.01;
     if (agi < fig) {
         return agi * (.05 + multiple_modifier);
@@ -182,7 +198,13 @@ function k_6(agi, fig, multiple) {
     }
 }
 
-function preschool(agi, fig, multiple) {
+/*
+ * k_6Calc returns a family tuition value for preschool tuition based on:
+ *  - the family's adjusted gross income
+ *  - federal income guidelines
+ *  - whether they have multiple children attending preschool at LSA (1 or 0)
+ */
+function preschoolCalc(agi, fig, multiple) {
     var multiple_modifier = multiple * 0.01;
     if (agi < fig) {
         return agi * (.06 + multiple_modifier);
@@ -198,7 +220,12 @@ function preschool(agi, fig, multiple) {
     }
 }
 
-function toggle() {
+/*
+ * toggleCalc toggles whether the calcDiv or resultsDiv is on display.
+ * Triggered either directly by clicking the <button> with ID calcAgain,
+ * or indirectly by clicking the <button> with ID calculateButton
+ */
+function toggleCalc() {
     document.getElementById("calcDiv").toggleAttribute("hidden");
     document.getElementById("resultsDiv").toggleAttribute("hidden");
 }
